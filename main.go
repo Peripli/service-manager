756d8f7fba9a1af4c926507d0b9c12dfce1875fb
Nikolay Mateev <nikolay.mateev@sap.com>
2018-04-24 18:19:15 +0300

    Tweaks

--- main.go
+++ main.go
@@ -1,38 +1,61 @@
+/*
+ * Copyright 2018 The Service Manager Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package main
 
 import (
 	"context"
-
-	"os"
-	"os/signal"
-
 	"github.com/Peripli/service-manager/env"
-	"github.com/Peripli/service-manager/sm"
-	"github.com/sirupsen/logrus"
+	"github.com/Peripli/service-manager/server"
+	"fmt"
+	"github.com/Peripli/service-manager/storage"
+	"github.com/Peripli/service-manager/storage/postgres"
+	"github.com/Peripli/service-manager/api"
+	cfenv "github.com/Peripli/service-manager/cmd/cf/env"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	handleInterrupts(ctx, cancel)
-
-	srv, err := sm.NewServer(ctx, env.Default())
+	ctx := context.Background()
+	server, err := CreateServer(ctx, cfenv.New(env.Default()))
 	if err != nil {
-		logrus.Fatal("Error creating the server", err)
+		panic(err)
 	}
-	srv.Run(ctx)
+	server.Run(ctx)
 }
 
-func handleInterrupts(ctx context.Context, cancelFunc context.CancelFunc) {
-	term := make(chan os.Signal)
-	signal.Notify(term, os.Interrupt)
-	go func() {
-		select {
-		case <-term:
-			logrus.Error("Received OS interrupt, exiting gracefully...")
-			cancelFunc()
-		case <-ctx.Done():
-			return
-		}
-	}()
+// CreateServer creates service manager server
+func CreateServer(ctx context.Context, serverEnv server.Environment) (*server.Server, error) {
+	config, err := server.NewConfiguration(serverEnv)
+	if err != nil {
+		return nil, fmt.Errorf("Error loading configuration: %v", err)
+	}
+
+	if err := config.Validate(); err != nil {
+		return nil, fmt.Errorf("Configuration validation failed: %v", err)
+	}
+
+	storage, err := storage.Use(ctx, postgres.Storage, config.DbURI)
+	if err != nil {
+		return nil, fmt.Errorf("Error using storage: %v", err)
+	}
+	defaultAPI := api.Default(storage)
+
+	srv, err := server.New(defaultAPI, config)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating server: %v", err)
+	}
+	return srv, nil
 }
