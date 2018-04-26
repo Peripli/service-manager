3364115e8bcf37c73741789c3509c1f513d9e814
Georgi Farashev <georgifarashev@gmail.com>
2018-04-26 20:56:10 +0300

    Add bind/unbind tests

--- test/osb_test/osb_test.go
+++ test/osb_test/osb_test.go
@@ -16,6 +16,7 @@
 package osb_test
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 	"net/http/httptest"
@@ -33,31 +34,37 @@ import (
 type object common.Object
 type array common.Array
 
+func setResponse(rw http.ResponseWriter, status int, message string) {
+	rw.WriteHeader(status)
+	rw.Write([]byte(message))
+}
+
 func createBrokerRouter() *mux.Router {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/good/v2/catalog", func(rw http.ResponseWriter, req *http.Request) {
-		rw.Write([]byte(`{"services": [{ "name":"sv1" }]}`))
+		setResponse(rw, http.StatusOK, `{"services": [{ "name":"sv1" }]}`)
 	})
+
 	router.HandleFunc("/good/v2/service_instances/{instance_id}", func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(http.StatusCreated)
-		rw.Write([]byte(`{}`))
+		setResponse(rw, http.StatusCreated, "{}")
 	}).Methods("PUT")
+
 	router.HandleFunc("/good/v2/service_instances/{instance_id}", func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(http.StatusOK)
-		rw.Write([]byte(`{}`))
-	}).Methods("DELETE")
-	router.HandleFunc("/good/v2/service_instances/{instance_id}/service_bindings/{binding_id}", func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(http.StatusCreated)
-		rw.Write([]byte(`{}`))
-	}).Methods("PUT")
-	router.HandleFunc("/good/v2/service_instances/{instance_id}/service_bindings/{binding_id}", func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(http.StatusOK)
-		rw.Write([]byte(`{}`))
+		setResponse(rw, http.StatusOK, "{}")
 	}).Methods("DELETE")
 
-	router.HandleFunc("/bad/v2/catalog", func(rw http.ResponseWriter, req *http.Request) {
-		rw.Write([]byte(`{"services": [{ "name":"sv1" }]}`))
+	router.HandleFunc("/good/v2/service_instances/{instance_id}/service_bindings/{binding_id}", func(rw http.ResponseWriter, req *http.Request) {
+		response := fmt.Sprintf(`{"credentials": {"instance_id": "%s" , "binding_id": "%s"}}`, mux.Vars(req)["instance_id"], mux.Vars(req)["binding_id"])
+		setResponse(rw, http.StatusCreated, response)
+	}).Methods("PUT")
+
+	router.HandleFunc("/good/v2/service_instances/{instance_id}/service_bindings/{binding_id}", func(rw http.ResponseWriter, req *http.Request) {
+		setResponse(rw, http.StatusOK, "{}")
+	}).Methods("DELETE")
+
+	router.PathPrefix("/bad/").HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		setResponse(rw, http.StatusNotAcceptable, `{"description": "expected error"}`)
 	})
 
 	return router
@@ -68,6 +75,12 @@ func registerBroker(brokerJSON object, SM *httpexpect.Expect) string {
 		WithJSON(brokerJSON).
 		Expect().Status(http.StatusCreated).JSON().Object()
 	return reply.Value("id").String().Raw()
+}
+
+func assertBadBrokerError(req *httpexpect.Request) {
+	body := req.Expect().Status(http.StatusNotAcceptable).JSON().Object()
+	body.ContainsKey("description")
+	body.Value("description").Equal("expected error")
 }
 
 // TestOSB tests for OSB API
@@ -116,11 +129,19 @@ var _ = Describe("Service Manager OSB API", func() {
 	})
 
 	Describe("Catalog", func() {
-		It("should get catalog", func() {
-			resp := SM.GET("/v1/osb/"+goodBrokerID+"/v2/catalog").WithHeader("X-Broker-API-Version", "2.13").
-				Expect().Status(http.StatusOK).JSON().Object()
+		Context("when call to working broker", func() {
+			It("should get catalog", func() {
+				resp := SM.GET("/v1/osb/"+goodBrokerID+"/v2/catalog").WithHeader("X-Broker-API-Version", "2.13").
+					Expect().Status(http.StatusOK).JSON().Object()
 
-			resp.ContainsKey("services")
+				resp.ContainsKey("services")
+			})
+		})
+		Context("when call to broken broker", func() {
+			It("should get error", func() {
+				assertBadBrokerError(
+					SM.GET("/v1/osb/"+badBrokerID+"/v2/catalog").WithHeader("X-Broker-API-Version", "2.13"))
+			})
 		})
 	})
 
@@ -134,7 +155,13 @@ var _ = Describe("Service Manager OSB API", func() {
 				resp.NotEmpty()
 			})
 		})
-
+		Context("when call to broken broker", func() {
+			It("should get error", func() {
+				assertBadBrokerError(
+					SM.PUT("/v1/osb/"+badBrokerID+"/v2/service_instances/12345").WithHeader("X-Broker-API-Version", "2.13").
+						WithJSON(dummyService))
+			})
+		})
 		Context("when call to missing broker", func() {
 			It("provision fails", func() {
 				resp := SM.PUT("/v1/osb/missing_broker_id/v2/service_instances/12345").WithHeader("X-Broker-API-Version", "2.13").
@@ -156,7 +183,13 @@ var _ = Describe("Service Manager OSB API", func() {
 					Expect().Status(http.StatusOK).JSON().Object()
 			})
 		})
-
+		Context("when call to broken broker", func() {
+			It("should get error", func() {
+				assertBadBrokerError(
+					SM.DELETE("/v1/osb/"+badBrokerID+"/v2/service_instances/12345").WithHeader("X-Broker-API-Version", "2.13").
+						WithQueryObject(dummyService))
+			})
+		})
 		Context("when call to missing broker", func() {
 			It("deprovision fails", func() {
 				resp := SM.DELETE("/v1/osb/missing_broker_id/v2/service_instances/12345").WithHeader("X-Broker-API-Version", "2.13").
@@ -166,6 +199,44 @@ var _ = Describe("Service Manager OSB API", func() {
 				resp.ContainsKey("description").
 					Value("description").String().
 					Equal("Could not find broker with id: missing_broker_id")
+			})
+		})
+	})
+
+	Describe("Bind", func() {
+		Context("when broker is working properly", func() {
+			It("should be successfull", func() {
+				resp := SM.PUT("/v1/osb/"+goodBrokerID+"/v2/service_instances/iid/service_bindings/bid").WithHeader("X-Broker-API-Version", "2.13").
+					WithJSON(dummyService).
+					Expect().Status(http.StatusCreated).JSON().Object()
+				credentials := resp.Value("credentials").Object()
+				credentials.Value("instance_id").String().Equal("iid")
+				credentials.Value("binding_id").String().Equal("bid")
+			})
+		})
+		Context("when broker is not working properly", func() {
+			It("should fail", func() {
+				assertBadBrokerError(
+					SM.PUT("/v1/osb/"+badBrokerID+"/v2/service_instances/iid/service_bindings/bid").WithHeader("X-Broker-API-Version", "2.13").
+						WithJSON(dummyService))
+			})
+		})
+	})
+
+	Describe("Unbind", func() {
+		Context("when trying to delete binding", func() {
+			It("should be successfull", func() {
+				SM.DELETE("/v1/osb/"+goodBrokerID+"/v2/service_instances/iid/service_bindings/bid").WithHeader("X-Broker-API-Version", "2.13").
+					WithQueryObject(dummyService).
+					Expect().Status(http.StatusOK).JSON().Object()
+
+			})
+		})
+		Context("for brokern broker", func() {
+			It("should return error", func() {
+				assertBadBrokerError(
+					SM.DELETE("/v1/osb/"+badBrokerID+"/v2/service_instances/iid/service_bindings/bid").WithHeader("X-Broker-API-Version", "2.13").
+						WithQueryObject(dummyService))
 			})
 		})
 	})
