8fd79950aeebca41de957f423cdcc9b47d4c554b
Dimitar Panayotov <dimitar.panayotov@sap.com>
2018-09-05 11:37:04 +0300

    Use own recovery middleware

--- api/filters/recovery.go
+++ api/filters/recovery.go
@@ -1,23 +1,30 @@
 package filters
 
 import (
+	"net/http"
+	"runtime/debug"
+
 	"github.com/Peripli/service-manager/pkg/log"
-	"github.com/gorilla/handlers"
+	"github.com/Peripli/service-manager/pkg/util"
 	"github.com/gorilla/mux"
 )
 
 // NewRecoveryMiddleware returns a standard mux middleware that provides panic recovery
 func NewRecoveryMiddleware() mux.MiddlewareFunc {
-	return handlers.RecoveryHandler(
-		handlers.PrintRecoveryStack(true),
-		handlers.RecoveryLogger(&recoveryHandlerLogger{}),
-	)
-}
-
-type recoveryHandlerLogger struct {
-}
-
-// PrintLn prints panic message and stack to error output
-func (r *recoveryHandlerLogger) Println(args ...interface{}) {
-	log.D().Errorln(args...)
+	return func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					httpError := &util.HTTPError{
+						StatusCode:  http.StatusInternalServerError,
+						Description: "Internal Server Error",
+					}
+					util.WriteError(httpError, w)
+					debug.PrintStack()
+					log.C(r.Context()).Error(err)
+				}
+			}()
+			handler.ServeHTTP(w, r)
+		})
+	}
 }
