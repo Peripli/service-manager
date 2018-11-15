c06cba6ed82255fc577377d9ef8e5be209237d6a
Peter Dotchev <dotchev@gmail.com>
2018-11-15 14:22:45 +0200

    Use last tag as version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,7 +5,10 @@ import "github.com/sirupsen/logrus"
 // GitCommit is the commit id, injected by the build
 var GitCommit string
 
+// Version is the SM version, injected by the build
+var Version string
+
 // Log writes the Service Manager version info in the log
 func Log() {
-	logrus.Infof("Service Manager Version: %s", GitCommit)
+	logrus.Infof("Service Manager Version: %s (%s)", Version, GitCommit)
 }
