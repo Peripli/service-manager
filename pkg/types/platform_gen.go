bd95c4ed6aee85348cf8e9a9da926801f5b6732e
Peter Dotchev <dotchev@gmail.com>
2019-05-10 14:29:35 +0300

    Fix race condition - do not modify during marshal

--- pkg/types/platform_gen.go
+++ pkg/types/platform_gen.go
@@ -37,8 +37,10 @@ func (e *Platform) MarshalJSON() ([]byte, error) {
 		*E
 		CreatedAt *string `json:"created_at,omitempty"`
 		UpdatedAt *string `json:"updated_at,omitempty"`
+		Labels    Labels  `json:"labels,omitempty"`
 	}{
-		E: (*E)(e),
+		E:      (*E)(e),
+		Labels: e.Labels,
 	}
 	if !e.CreatedAt.IsZero() {
 		str := util.ToRFCFormat(e.CreatedAt)
