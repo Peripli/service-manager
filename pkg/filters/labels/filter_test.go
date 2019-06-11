7a2799b175af383f640e1c438f2336ef9bb2516f
Nikolay <pankrator@users.noreply.github.com>
2019-06-11 18:40:54 +0300

    Filter for protected labels on resources (#298)
    * Deny protected labels operations on resources
    
    * Move forbidden label fitler to filter package
    
    * Add forbidden labels filter test
    
    * Format files
    
    * Add config validation for forbidden label operation filter
    
    * Remove pointer receiver on config
    
    * Fix validation
    
    * Rework filter settings. Register filter. Add tests
    
    * Reorder filters

---
+++ pkg/filters/labels/filter_test.go
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2018 The Service Manager Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package labels
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/Peripli/service-manager/pkg/util"
+	"github.com/Peripli/service-manager/pkg/web"
+	"github.com/Peripli/service-manager/pkg/web/webfakes"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestLabelsFilter(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Labels filter Suite")
+}
+
+var _ = Describe("Forbidden label operations filter", func() {
+	var filter *ForibiddenLabelOperationsFilter
+	var handler *webfakes.FakeHandler
+	var protectedLabels []string
+
+	BeforeEach(func() {
+		protectedLabels = []string{"forbidden"}
+	})
+
+	JustBeforeEach(func() {
+		handler = &webfakes.FakeHandler{}
+		filter = NewForbiddenLabelOperationsFilter(protectedLabels)
+	})
+
+	Context("POST", func() {
+		When("entity has forbidden labels", func() {
+			It("should return 400", func() {
+				req := mockedRequest(http.MethodPost, `{"labels": {"forbidden": ["forbidden_value"]}}`)
+				_, err := filter.Run(req, handler)
+				httpErr, ok := err.(*util.HTTPError)
+				Expect(ok).To(BeTrue())
+				Expect(httpErr.StatusCode).To(Equal(http.StatusBadRequest))
+				Expect(httpErr.Description).To(ContainSubstring("Set/Add values for label forbidden is not allowed"))
+				Expect(handler.HandleCallCount()).To(Equal(0))
+			})
+		})
+
+		When("entity has no forbidden labels", func() {
+			It("should call next filter in chain", func() {
+				req := mockedRequest(http.MethodPost, `{"labels": {"allowed": ["value"]}}`)
+				_, err := filter.Run(req, handler)
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(handler.HandleCallCount()).To(Equal(1))
+			})
+		})
+
+		When("there are no protected labels specified", func() {
+			BeforeEach(func() {
+				protectedLabels = []string{}
+			})
+			It("should call next filter in chain", func() {
+				req := mockedRequest(http.MethodPost, `{"labels": {"forbidden": ["forbidden_value"]}}`)
+				_, err := filter.Run(req, handler)
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(handler.HandleCallCount()).To(Equal(1))
+			})
+		})
+
+		When("no labels are provided", func() {
+			It("should call next filter in chain", func() {
+				req := mockedRequest(http.MethodPost, `{}`)
+				_, err := filter.Run(req, handler)
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(handler.HandleCallCount()).To(Equal(1))
+			})
+		})
+
+		When("empty labels are provided", func() {
+			It("should call next filter in chain", func() {
+				req := mockedRequest(http.MethodPost, `{"labels":nil}`)
+				_, err := filter.Run(req, handler)
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(handler.HandleCallCount()).To(Equal(1))
+			})
+		})
+
+		When("when labels is invalid json", func() {
+			It("should return error", func() {
+				req := mockedRequest(http.MethodPost, `{"labels": "invalid"}`)
+				_, err := filter.Run(req, handler)
+				Expect(err).Should(HaveOccurred())
+				Expect(err.Error()).Should(ContainSubstring("Failed to decode"))
+				Expect(handler.HandleCallCount()).To(Equal(0))
+			})
+		})
+	})
+
+	Context("PATCH", func() {
+		When("entity is modified with forbidden labels", func() {
+			It("should return 400", func() {
+				req := mockedRequest(http.MethodPatch, `{"labels": [{"op": "add", "key":"forbidden", "values":["forbidden_value"]}]}`)
+				_, err := filter.Run(req, handler)
+				httpErr, ok := err.(*util.HTTPError)
+				Expect(ok).To(BeTrue())
+				Expect(httpErr.StatusCode).To(Equal(http.StatusBadRequest))
+				Expect(httpErr.Description).To(ContainSubstring("Modifying is not allowed for label forbidden"))
+				Expect(handler.HandleCallCount()).To(Equal(0))
+			})
+		})
+
+		When("there are no protected labels specified", func() {
+			BeforeEach(func() {
+				protectedLabels = []string{}
+			})
+			It("should call next filter in chain", func() {
+				req := mockedRequest(http.MethodPatch, `{"labels": [{"op":"add", "key":"forbidden", "values":["forbidden_value"]}]}`)
+				_, err := filter.Run(req, handler)
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(handler.HandleCallCount()).To(Equal(1))
+			})
+		})
+
+		When("when labels is invalid json", func() {
+			It("should return error", func() {
+				req := mockedRequest(http.MethodPatch, `{"labels": "invalid"}`)
+				_, err := filter.Run(req, handler)
+				Expect(err).Should(HaveOccurred())
+				Expect(err.Error()).Should(ContainSubstring("Failed to decode"))
+				Expect(handler.HandleCallCount()).To(Equal(0))
+			})
+		})
+
+		When("entity has no forbidden labels", func() {
+			It("should call next filter in chain", func() {
+				req := mockedRequest(http.MethodPatch, `{"labels": [{"op":"add", "key":"allowed", "values":["value"]}]}`)
+				_, err := filter.Run(req, handler)
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(handler.HandleCallCount()).To(Equal(1))
+			})
+		})
+	})
+})
+
+func mockedRequest(method, json string) *web.Request {
+	req, err := http.NewRequest(method, "", nil)
+	Expect(err).ShouldNot(HaveOccurred())
+	return &web.Request{Request: req, Body: []byte(json)}
+}
