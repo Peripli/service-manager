b0019ddc6c1b24aea339d3737ea1cfca13317dd6
Nikolay <pankrator@users.noreply.github.com>
2019-06-13 16:55:03 +0300

    Do not return empty labels when no labels are present in database (#300)

---
+++ test/storage_test/storage_test.go
@@ -0,0 +1,52 @@
+package storage_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Peripli/service-manager/pkg/types"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/Peripli/service-manager/test/common"
+)
+
+func TestStorage(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Storage Suite")
+}
+
+var _ = Describe("Test", func() {
+	var ctx *common.TestContext
+
+	BeforeEach(func() {
+		ctx = common.NewTestContextBuilder().Build()
+	})
+
+	AfterEach(func() {
+		ctx.Cleanup()
+	})
+
+	Context("when resource is created without labels", func() {
+		var platform types.Object
+		var err error
+		BeforeEach(func() {
+			platform, err = ctx.SMRepository.Create(context.Background(), &types.Platform{
+				Base: types.Base{
+					ID: "id",
+				},
+				Description: "desc",
+				Name:        "platform_name",
+				Type:        "cloudfoundry",
+			})
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		It("should be fetched from storage without any labels", func() {
+			obj, err := ctx.SMRepository.Get(context.Background(), types.PlatformType, platform.GetID())
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(obj.GetLabels()).To(HaveLen(0))
+		})
+	})
+})
