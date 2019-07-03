02185ffb1fe44577f2fd3babb93c5b1f45d7e9ec
Dimitar Petrov <buldozer9993@gmail.com>
2019-07-03 12:17:13 +0300

    Add tests for UserFromContext

--- pkg/web/web_suite_test.go
+++ pkg/web/web_suite_test.go
@@ -16,7 +16,11 @@
 
 package web_test
 
-import "testing"
+import (
+	"context"
+	"github.com/Peripli/service-manager/pkg/web"
+	"testing"
+)
 import . "github.com/onsi/ginkgo"
 import . "github.com/onsi/gomega"
 
@@ -24,3 +28,33 @@ func TestWeb(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Web Suite")
 }
+
+var _ = Describe("Context keys", func() {
+
+	Context("when valid user is in context", func() {
+		It("returns user and ok", func() {
+			ctx := web.ContextWithUser(context.TODO(), &web.UserContext{})
+			userContext, ok := web.UserFromContext(ctx)
+			Expect(userContext).ToNot(BeNil())
+			Expect(ok).To(BeTrue())
+		})
+	})
+
+	Context("when nil user is in context", func() {
+		It("returns nil and not ok", func() {
+			ctx := web.ContextWithUser(context.TODO(),nil)
+			userContext, ok := web.UserFromContext(ctx)
+			Expect(userContext).To(BeNil())
+			Expect(ok).To(BeFalse())
+		})
+	})
+
+	Context("when user is not in context", func() {
+		It("returns nil and not ok", func() {
+			userContext, ok := web.UserFromContext(context.TODO())
+			Expect(userContext).To(BeNil())
+			Expect(ok).To(BeFalse())
+		})
+	})
+
+})
