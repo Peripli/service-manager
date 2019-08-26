7ffd05197433e8a36fb4fbfc44b72f0d1b3a3bb4
Dimitar Petrov <buldozer9993@gmail.com>
2019-08-26 14:38:52 +0300

    Add tests

--- api/healthcheck/platform_indicator.go
+++ api/healthcheck/platform_indicator.go
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2018 The Service Manager Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package healthcheck
 
 import (
@@ -8,8 +24,6 @@ import (
 	"github.com/Peripli/service-manager/pkg/types"
 	"github.com/Peripli/service-manager/storage"
 )
-
-//TODO: Add Tests
 
 // NewPlatformIndicator returns new health indicator for platforms of given type
 func NewPlatformIndicator(ctx context.Context, repository storage.Repository, platformType string) health.Indicator {
@@ -33,13 +47,13 @@ func (pi *platformIndicator) Name() string {
 
 // Status returns status of the health check
 func (pi *platformIndicator) Status() (interface{}, error) {
-	cfCriteria := query.Criterion{
+	typeCriteria := query.Criterion{
 		LeftOp:   "type",
 		Operator: query.EqualsOperator,
 		RightOp:  []string{pi.platformType},
 		Type:     query.FieldQuery,
 	}
-	objList, err := pi.repository.List(pi.ctx, types.PlatformType, cfCriteria)
+	objList, err := pi.repository.List(pi.ctx, types.PlatformType, typeCriteria)
 	if err != nil {
 		return nil, fmt.Errorf("could not fetch platforms health from storage: %v", err)
 	}
