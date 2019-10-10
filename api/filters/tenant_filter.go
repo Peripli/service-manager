322cba16bbbfd97c4425f6aa42e60566aa7facc8
kkabakchiev <kiril.kabakchiev@sap.com>
2019-10-10 16:00:19 +0300

    reorder

--- api/filters/tenant_filter.go
+++ api/filters/tenant_filter.go
@@ -114,12 +114,6 @@ func (f *TenantFilter) Run(request *web.Request, next web.Handler) (*web.Respons
 
 	ctx := request.Context()
 
-	userContext, found := web.UserFromContext(ctx)
-	if !found {
-		log.C(ctx).Infof("No user found in user context. Proceeding with empty tenant ID value...")
-		return next.Handle(request)
-	}
-
 	tenant, err := f.ExtractTenant(request)
 	if err != nil {
 		return nil, err
@@ -129,6 +123,11 @@ func (f *TenantFilter) Run(request *web.Request, next web.Handler) (*web.Respons
 		return next.Handle(request)
 	}
 
+	userContext, found := web.UserFromContext(ctx)
+	if !found {
+		log.C(ctx).Infof("No user found in user context. Proceeding with empty tenant ID value...")
+		return next.Handle(request)
+	}
 	if userContext.AccessLevel == web.GlobalAccess {
 		log.C(ctx).Infof("Access level is Global. Proceeding and ignoring tenant ID value %s...", tenant)
 		return next.Handle(request)
