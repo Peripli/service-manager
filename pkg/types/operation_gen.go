5d40b20634c2b8b410b505c6c4284532ba368b09
Nikolay Mateev <bgdunkers@gmail.com>
2019-11-19 21:31:20 +0200

    Introduce Operation resource (#352)

---
+++ pkg/types/operation_gen.go
@@ -0,0 +1,64 @@
+// GENERATED. DO NOT MODIFY!
+
+package types
+
+import (
+	"encoding/json"
+
+	"github.com/Peripli/service-manager/pkg/util"
+)
+
+const OperationType ObjectType = "types.Operation"
+
+type Operations struct {
+	Operations []*Operation `json:"operations"`
+}
+
+func (e *Operations) Add(object Object) {
+	e.Operations = append(e.Operations, object.(*Operation))
+}
+
+func (e *Operations) ItemAt(index int) Object {
+	return e.Operations[index]
+}
+
+func (e *Operations) Len() int {
+	return len(e.Operations)
+}
+
+func (e *Operation) GetType() ObjectType {
+	return OperationType
+}
+
+// MarshalJSON override json serialization for http response
+func (e *Operation) MarshalJSON() ([]byte, error) {
+	type E Operation
+	toMarshal := struct {
+		*E
+		CreatedAt *string `json:"created_at,omitempty"`
+		UpdatedAt *string `json:"updated_at,omitempty"`
+		Labels    Labels  `json:"labels,omitempty"`
+	}{
+		E:      (*E)(e),
+		Labels: e.Labels,
+	}
+	if !e.CreatedAt.IsZero() {
+		str := util.ToRFCNanoFormat(e.CreatedAt)
+		toMarshal.CreatedAt = &str
+	}
+	if !e.UpdatedAt.IsZero() {
+		str := util.ToRFCNanoFormat(e.UpdatedAt)
+		toMarshal.UpdatedAt = &str
+	}
+	hasNoLabels := true
+	for key, values := range e.Labels {
+		if key != "" && len(values) != 0 {
+			hasNoLabels = false
+			break
+		}
+	}
+	if hasNoLabels {
+		toMarshal.Labels = nil
+	}
+	return json.Marshal(toMarshal)
+}
