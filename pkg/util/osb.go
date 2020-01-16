f0364766bbdbb89b2dd5fb9f73d34b7e8e2ed58d
NickyMateev <bgdunkers@gmail.com>
2020-01-16 06:43:57 +0200

    Add godoc

--- pkg/util/osb.go
+++ pkg/util/osb.go
@@ -2,6 +2,7 @@ package util
 
 import osbc "github.com/pmorie/go-open-service-broker-client/v2"
 
+// NewOSBCient returns a function which constructs an OSB client based on a provided configuration
 func NewOSBClient(skipSsl bool) osbc.CreateFunc {
 	return func(configuration *osbc.ClientConfiguration) (osbc.Client, error) {
 		configuration.Insecure = skipSsl
