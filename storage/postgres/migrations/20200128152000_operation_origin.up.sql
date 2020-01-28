2438cfc669e05ba951229ea4b4d4d4b33b7db7dc
desislavaa <dess.95@abv.bg>
2020-01-28 23:05:59 +0200

    Modify migration

--- storage/postgres/migrations/20200128152000_operation_origin.up.sql
+++ storage/postgres/migrations/20200128152000_operation_origin.up.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
 ALTER TABLE operations ADD COLUMN origin varchar(18) NOT NULL DEFAULT 'external';
+UPDATE operations SET origin = 'internal' WHERE resource_type != '/v1/service_instances';
 
 COMMIT;
