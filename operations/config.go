454a11ccacf77469b45eea4e84fc2577284b1f48
desislavaa <dess.95@abv.bg>
2020-01-30 12:39:39 +0200

    Fix linter

--- operations/config.go
+++ operations/config.go
@@ -22,13 +22,13 @@ import (
 )
 
 const (
-	minTimePeriod              = time.Nanosecond
-	
+	minTimePeriod = time.Nanosecond
+
 	defaultMarkOrphansInterval = 24 * time.Hour
 	defaultJobTimeout          = 7*24*time.Hour - 1*time.Hour
 
-	defaultCleanupInterval     = 1 * time.Hour
-	defaultExpirationTime      = 7 * 24 * time.Hour
+	defaultCleanupInterval = 1 * time.Hour
+	defaultExpirationTime  = 7 * 24 * time.Hour
 )
 
 // Settings type to be loaded from the environment
