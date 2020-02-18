056b15ef0376743f891118c4c47438d9d21540cf
i042428 <eyal.nathan@sap.com>
2020-02-18 15:53:01 +0200

    Expose default visibility function

--- pkg/sm/sm.go
+++ pkg/sm/sm.go
@@ -505,8 +505,8 @@ func (smb *ServiceManagerBuilder) EnableMultitenancy(labelKey string, extractTen
 	multitenancyFilters := filters.NewMultitenancyFilters(labelKey, extractTenantFunc)
 	smb.RegisterFiltersAfter(filters.ProtectedLabelsFilterName, multitenancyFilters...)
 	smb.RegisterFilters(
-		filters.NewServiceInstanceVisibilityFilter(smb.Storage, getDefaultInstanceVisibilityFunc(labelKey)),
-		filters.NewServiceBindingVisibilityFilter(smb.Storage, getDefaultInstanceVisibilityFunc(labelKey)),
+		filters.NewServiceInstanceVisibilityFilter(smb.Storage, DefaultInstanceVisibilityFunc(labelKey)),
+		filters.NewServiceBindingVisibilityFilter(smb.Storage, DefaultInstanceVisibilityFunc(labelKey)),
 	)
 
 	smb.RegisterPlugins(osb.NewCheckInstanceOwnershipPlugin(smb.Storage, labelKey))
@@ -526,7 +526,7 @@ func (smb *ServiceManagerBuilder) Security() *SecurityBuilder {
 	return smb.securityBuilder.Reset()
 }
 
-func getDefaultInstanceVisibilityFunc(labelKey string) func(req *web.Request, repository storage.Repository) (metadata *filters.InstanceVisibilityMetadata, err error) {
+func DefaultInstanceVisibilityFunc(labelKey string) func(req *web.Request, repository storage.Repository) (metadata *filters.InstanceVisibilityMetadata, err error) {
 	return func(req *web.Request, repository storage.Repository) (metadata *filters.InstanceVisibilityMetadata, err error) {
 		tenantID := query.RetrieveFromCriteria(labelKey, query.CriteriaForContext(req.Context())...)
 		if tenantID == "" {
