6675f1c18c75dbfc1bdf8c4bfdb8bddda16d98a0
Dimitar Panayotov <dpanayotov94@gmail.com>
2020-02-28 09:44:54 +0200

    Secured objects integrity (#440)

---
+++ storage/postgres/migrations/20200219095000_credentials_integrity.down.sql
@@ -0,0 +1,7 @@
+BEGIN;
+
+ALTER TABLE brokers DROP COLUMN IF EXISTS integrity;
+ALTER TABLE platforms DROP COLUMN IF EXISTS integrity;
+ALTER TABLE service_bindings DROP COLUMN IF EXISTS integrity;
+
+COMMIT;
