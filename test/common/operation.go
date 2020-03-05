ea636defa46dabb783ef007f5fd28c61969e1f2e
NickyMateev <bgdunkers@gmail.com>
2020-03-05 09:28:11 +0200

    Adjust job timeout

--- test/common/operation.go
+++ test/common/operation.go
@@ -91,7 +91,7 @@ func VerifyResourceDoesNotExist(smClient *SMExpect, expectations ResourceExpecta
 }
 
 func VerifyOperationExists(ctx *TestContext, operationURL string, expectations OperationExpectations) (string, string) {
-	timeoutDuration := 15 * time.Second
+	timeoutDuration := 20 * time.Second
 	timeout := time.After(timeoutDuration)
 	ticker := time.Tick(100 * time.Millisecond)
 	for {
