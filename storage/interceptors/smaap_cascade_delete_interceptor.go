7481a7040450eb15ccb2df07c4f168182b00a286
Pavel Maliy <pavel.maliy@sap.com>
2020-05-14 22:48:11 +0300

    moshe

--- storage/interceptors/smaap_cascade_delete_interceptor.go
+++ storage/interceptors/smaap_cascade_delete_interceptor.go
@@ -55,7 +55,7 @@ func (co *cascadeOperationCreateInterceptor) OnTxCreate(f storage.InterceptCreat
 			co.TenantIdentifier,
 		}
 
-		ops, err := opUtil.GetOneLevelCascadeOperations(ctx, operation, storage)
+		ops, err := opUtil.GetAllLevelsCascadeOperations(ctx, operation, storage)
 		if err != nil {
 			return nil, err
 		}
