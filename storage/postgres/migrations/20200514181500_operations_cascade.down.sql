53c92bf4cbbed0ef2c16705f2d920e1822c567ce
Pavel Maliy <pavel.maliy@sap.com>
2020-05-23 01:25:20 +0300

    fix tests

--- storage/postgres/migrations/20200514181500_operations_cascade.down.sql
+++ storage/postgres/migrations/20200514181500_operations_cascade.down.sql
@@ -3,7 +3,4 @@ BEGIN;
 ALTER TABLE operations DROP COLUMN cascade_root_id;
 ALTER TABLE operations DROP COLUMN parent_id;
 
-DROP TYPE operation_state;
-CREATE TYPE operation_state AS ENUM ('succeeded', 'failed', 'pending');
-
 COMMIT;
--- storage/postgres/migrations/20200514181500_operations_cascade.up.sql
+++ storage/postgres/migrations/20200514181500_operations_cascade.up.sql
@@ -3,6 +3,5 @@ BEGIN;
 ALTER TABLE operations ADD COLUMN IF NOT EXISTS cascade_root_id varchar(100);
 ALTER TABLE operations ADD COLUMN IF NOT EXISTS parent_id varchar(100);
 
-ALTER TYPE operation_state ADD VALUE 'pending';
-
 COMMIT;
+
