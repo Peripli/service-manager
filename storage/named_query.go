cff74038925ea2041c1e183b6c719238243d9aae
Pavel Maliy <pavel.maliy@sap.com>
2020-05-25 22:32:42 +0300

    first query

--- storage/named_query.go
+++ storage/named_query.go
@@ -6,12 +6,20 @@ type NamedQuery int
 
 const (
 	QueryByMissingLabel NamedQuery = iota
+	QueryByExistingLabel
 )
 
 var namedQueries =  map[NamedQuery]string {
+
 	QueryByMissingLabel : `
 	SELECT * FROM {{.ENTITY_TABLE}}
 	WHERE NOT EXISTS
+	(SELECT ID FROM {{.LABELS_TABLE}} 
+				WHERE key=:key
+				AND {{.ENTITY_TABLE}}.{{.PRIMARY_KEY}} = {{.LABELS_TABLE}}.{{.REF_COLUMN}})`,
+	QueryByExistingLabel: `
+	SELECT * FROM {{.ENTITY_TABLE}}
+	WHERE EXISTS
 	(SELECT ID FROM {{.LABELS_TABLE}} 
 				WHERE key=:key
 				AND {{.ENTITY_TABLE}}.{{.PRIMARY_KEY}} = {{.LABELS_TABLE}}.{{.REF_COLUMN}})`,
