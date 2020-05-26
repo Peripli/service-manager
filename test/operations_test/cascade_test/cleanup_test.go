f083793c36c955ac5448a6c07d52da19d5758974
Pavel Maliy <pavel.maliy@sap.com>
2020-05-27 01:29:12 +0300

    test

--- test/operations_test/cascade_test/cleanup_test.go
+++ test/operations_test/cascade_test/cleanup_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Peripli/service-manager/pkg/types"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"net/http"
 	"time"
 )
 
@@ -30,6 +31,25 @@ var _ = Describe("cascade operations", func() {
 
 				return count
 			}, actionTimeout*11+pollCascade*11+cleanupInterval).Should(Equal(0))
+		})
+
+		It("multiple finished trees should be deleted", func() {
+
+		})
+
+		It("in_progress should not be deleted", func() {
+			registerBindingLastOPHandlers(brokerServer, http.StatusOK, types.IN_PROGRESS)
+			triggerCascadeOperation(context.Background(), types.TenantType, tenantID)
+
+			Eventually(func() int {
+				count, err := ctx.SMRepository.Count(
+					context.Background(),
+					types.OperationType,
+					queryForOperationsInTheSameTree)
+				Expect(err).NotTo(HaveOccurred())
+
+				return count
+			}, actionTimeout*11+pollCascade*11+cleanupInterval).Should(Equal(11))
 		})
 	})
 })
