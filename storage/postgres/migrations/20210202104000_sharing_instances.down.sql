39f734ce414e030d841c307da13968524bd84682
Tal Yaakov <tal.yaakov@sap.com>
2021-02-03 12:56:25 +0200

    Adding omitempty Shareable to service plan types

--- storage/postgres/migrations/20210202104000_sharing_instances.down.sql
+++ storage/postgres/migrations/20210202104000_sharing_instances.down.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
-ALTER TABLE service_instances   DROP COLUMN IF EXISTS referenced_instance_id;
-ALTER TABLE service_instances   DROP COLUMN IF EXISTS shareable;
-ALTER TABLE service_plans       DROP COLUMN IF EXISTS shareable;
+ALTER TABLE service_instances DROP COLUMN IF EXISTS referenced_instance_id;
+ALTER TABLE service_instances DROP COLUMN IF EXISTS shareable;
+ALTER TABLE service_plans DROP COLUMN IF EXISTS shareable;
 
 COMMIT;
 
--- storage/postgres/migrations/20210202104000_sharing_instances.up.sql
+++ storage/postgres/migrations/20210202104000_sharing_instances.up.sql
@@ -1,5 +1,9 @@
-ALTER TABLE service_instances   ADD COLUMN IF NOT EXISTS referenced_instance_id     varchar(250);
-ALTER TABLE service_instances   ADD COLUMN IF NOT EXISTS shareable                  boolean DEFAULT FALSE;
-ALTER TABLE service_plans       ADD COLUMN IF NOT EXISTS shareable                  boolean DEFAULT FALSE;
+BEGIN;
+
+ALTER TABLE service_instances ADD COLUMN IF NOT EXISTS referenced_instance_id varchar(250);
+ALTER TABLE service_instances ADD COLUMN IF NOT EXISTS shareable boolean DEFAULT FALSE;
+ALTER TABLE service_plans ADD COLUMN IF NOT EXISTS shareable boolean DEFAULT FALSE;
+
+COMMIT;
 
 
