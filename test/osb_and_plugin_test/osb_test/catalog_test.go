d69e43e2771b4e8e507b8569aefbd9c176c1d20b
i501080 <keren.lahav@sap.com>
2021-02-10 16:42:11 +0200

    remove F

--- test/osb_and_plugin_test/osb_test/catalog_test.go
+++ test/osb_and_plugin_test/osb_test/catalog_test.go
@@ -93,7 +93,7 @@ var _ = Describe("Catalog", func() {
 				resp.Path("$.services[*].plans[*].random_extension").Array().Contains("random_extension")
 			})
 
-			FIt("should return the SM offering id in the catalog metadata", func() {
+			It("should return the SM offering id in the catalog metadata", func() {
 				resp := ctx.SMWithBasic.GET(smUrlToSimpleBrokerCatalogBroker+"/v2/catalog").WithHeader(brokerAPIVersionHeaderKey, brokerAPIVersionHeaderValue).
 					Expect().
 					Status(http.StatusOK).JSON()
