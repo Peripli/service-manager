f0c6b2b72f1695e984513b3a4da625a3fb0ce669
epsilontal <epsilontal@live.com>
2021-04-05 15:35:33 +0300

    Validation before delete shared instance. If the instance is shared -> validates there are no references.

--- api/filters/sharing_instance_filter.go
+++ api/filters/sharing_instance_filter.go
@@ -48,70 +48,25 @@ func (*sharingInstanceFilter) Name() string {
 }
 
 func (f *sharingInstanceFilter) Run(req *web.Request, next web.Handler) (*web.Response, error) {
-	var reqServiceInstance types.ServiceInstance
-	var err error
-	err = util.BytesToObjectNoLabels(req.Body, &reqServiceInstance)
-
-	if err != nil {
-		return nil, err
+	switch req.Request.Method {
+	case http.MethodDelete:
+		return f.handleDeprovision(req, next)
+	case http.MethodPatch:
+		return f.handleServiceUpdate(req, next)
 	}
+	return nil, nil
 
-	if reqServiceInstance.Shared == nil {
-		return next.Handle(req)
+}
+
+func (*sharingInstanceFilter) FilterMatchers() []web.FilterMatcher {
+	return []web.FilterMatcher{
+		{
+			Matchers: []web.Matcher{
+				web.Path(web.ServiceInstancesURL + "/**"),
+				web.Methods(http.MethodPatch),
+			},
+		},
 	}
-
-	ctx := req.Context()
-	logger := log.C(ctx)
-
-	// Get instance from database
-	instanceID := req.PathParams["resource_id"]
-	byID := query.ByField(query.EqualsOperator, "id", instanceID)
-	dbInstanceObject, err := f.storageRepository.Get(ctx, types.ServiceInstanceType, byID)
-	if err != nil {
-		return nil, util.HandleStorageError(err, types.ServiceInstanceType.String())
-	}
-
-	persistedInstance := dbInstanceObject.(*types.ServiceInstance)
-
-	//we cannot use reqServiceInstance in this validation because the struct has default values (like "" for string type properties)
-	err = f.validateOnlySharedPropertyIsChanged(persistedInstance, &req.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	// Get plan object from database, on service_instance patch flow
-	planID := persistedInstance.ServicePlanID
-	byID = query.ByField(query.EqualsOperator, "id", planID)
-	planObject, err := f.storageRepository.Get(ctx, types.ServicePlanType, byID)
-	if err != nil {
-		return nil, util.HandleStorageError(err, types.ServicePlanType.String())
-	}
-	plan := planObject.(*types.ServicePlan)
-
-	if !plan.IsShareablePlan() {
-		return nil, &util.UnsupportedQueryError{
-			Message: "Plan is non-shared",
-		}
-	}
-
-	if persistedInstance.Shared != nil && *persistedInstance.Shared == *reqServiceInstance.Shared {
-		return nil, errors.New(fmt.Sprintf("The service instance is already at the desried state=%t", *reqServiceInstance.Shared))
-	}
-
-	// When un-sharing a service instance with references
-	if persistedInstance.Shared != nil && !*reqServiceInstance.Shared {
-		referencesList, err := f.getInstanceReferencesByID(persistedInstance.ID)
-
-		if err != nil {
-			logger.Errorf("Could not retrieve references of the service instance (%s): %v", instanceID, err)
-		}
-
-		if referencesList.Len() > 0 {
-			return nil, errors.New(fmt.Sprintf("Could not un-share the service instance. The service instance has %d references which should be deleted first", referencesList.Len()))
-		}
-	}
-
-	return next.Handle(req)
 }
 
 func (f *sharingInstanceFilter) validateOnlySharedPropertyIsChanged(persistedInstance *types.ServiceInstance, reqInstanceBytes *[]byte) error {
@@ -149,13 +104,112 @@ func (f *sharingInstanceFilter) getInstanceReferencesByID(instanceID string) (ty
 	return references, nil
 }
 
-func (*sharingInstanceFilter) FilterMatchers() []web.FilterMatcher {
-	return []web.FilterMatcher{
-		{
-			Matchers: []web.Matcher{
-				web.Path(web.ServiceInstancesURL + "/**"),
-				web.Methods(http.MethodPatch),
-			},
-		},
+func (f *sharingInstanceFilter) handleServiceUpdate(req *web.Request, next web.Handler) (*web.Response, error) {
+	var reqServiceInstance types.ServiceInstance
+	var err error
+	err = util.BytesToObjectNoLabels(req.Body, &reqServiceInstance)
+
+	if err != nil {
+		return nil, err
 	}
+
+	if reqServiceInstance.Shared == nil {
+		return next.Handle(req)
+	}
+
+	ctx := req.Context()
+	logger := log.C(ctx)
+
+	// Get instance from database
+	instanceID := req.PathParams["resource_id"]
+	persistedInstance, err := f.retrievePersistedInstanceByID(ctx, instanceID)
+	if err != nil {
+		return nil, util.HandleStorageError(err, types.ServiceInstanceType.String())
+	}
+
+	//we cannot use reqServiceInstance in this validation because the struct has default values (like "" for string type properties)
+	err = f.validateOnlySharedPropertyIsChanged(persistedInstance, &req.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get plan object from database, on service_instance patch flow
+	planID := persistedInstance.ServicePlanID
+	byID := query.ByField(query.EqualsOperator, "id", planID)
+	planObject, err := f.storageRepository.Get(ctx, types.ServicePlanType, byID)
+	if err != nil {
+		return nil, util.HandleStorageError(err, types.ServicePlanType.String())
+	}
+	plan := planObject.(*types.ServicePlan)
+
+	if !plan.IsShareablePlan() {
+		return nil, &util.UnsupportedQueryError{
+			Message: "Plan is non-shared",
+		}
+	}
+
+	if persistedInstance.Shared != nil && *persistedInstance.Shared == *reqServiceInstance.Shared {
+		return nil, errors.New(fmt.Sprintf("The service instance is already at the desried state=%t", *reqServiceInstance.Shared))
+	}
+
+	// When un-sharing a service instance with references
+	if persistedInstance.Shared != nil && !*reqServiceInstance.Shared {
+		referencesList, err := f.getInstanceReferencesByID(persistedInstance.ID)
+
+		if err != nil {
+			logger.Errorf("Could not retrieve references of the service instance (%s): %v", instanceID, err)
+		}
+
+		if referencesList.Len() > 0 {
+			errorMessage := fmt.Sprintf("could not delete the service instance. The service instance has %d references which should be deleted first", referencesList.Len())
+			return nil, &util.HTTPError{
+				ErrorType:   "BadRequest",
+				Description: errorMessage,
+				StatusCode:  http.StatusBadRequest,
+			}
+		}
+	}
+
+	return next.Handle(req)
+}
+
+func (f *sharingInstanceFilter) handleDeprovision(req *web.Request, next web.Handler) (*web.Response, error) {
+	ctx := req.Context()
+	logger := log.C(ctx)
+
+	instanceID := req.PathParams["resource_id"]
+
+	// Get instance from database
+	persistedInstance, err := f.retrievePersistedInstanceByID(ctx, instanceID)
+	if err != nil {
+		return nil, util.HandleStorageError(err, types.ServiceInstanceType.String())
+	}
+
+	if persistedInstance.Shared == newTrue() {
+		referencesList, err := f.getInstanceReferencesByID(persistedInstance.ID)
+		if err != nil {
+			logger.Errorf("Could not retrieve references of the service instance (%s): %v", instanceID, err)
+		}
+		if referencesList.Len() > 0 {
+			errorMessage := fmt.Sprintf("could not delete the service instance. The service instance has %d references which should be deleted first", referencesList.Len())
+			return nil, &util.HTTPError{
+				ErrorType:   "BadRequest",
+				Description: errorMessage,
+				StatusCode:  http.StatusBadRequest,
+			}
+		}
+	}
+	return next.Handle(req)
+}
+
+func (f *sharingInstanceFilter) retrievePersistedInstanceByID(ctx context.Context, instanceID string) (*types.ServiceInstance, error) {
+	byID := query.ByField(query.EqualsOperator, "id", instanceID)
+	dbInstanceObject, err := f.storageRepository.Get(ctx, types.ServiceInstanceType, byID)
+	persistedInstance := dbInstanceObject.(*types.ServiceInstance)
+	return persistedInstance, err
+}
+
+func newTrue() *bool {
+	b := true
+	return &b
 }
