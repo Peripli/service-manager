df0a938fb1ef11dce7311dc18adc26ade6ae3ecf
kerenlahav <45451976+kerenlahav@users.noreply.github.com>
2021-04-06 14:04:07 +0300

    provision timeout (#651)
    * provision timeout

---
+++ test/osb_and_plugin_test/provision_timeout_test/provision_timeout_test.go
@@ -0,0 +1,32 @@
+package provision_timeout
+
+import (
+	"github.com/Peripli/service-manager/pkg/web"
+	. "github.com/onsi/ginkgo"
+	"net/http"
+)
+
+var _ = Describe("Provision timeout", func() {
+	Context("when timeout has not reached", func() {
+		It("should succeed", func() {
+			brokerServer.ServiceInstanceHandler = parameterizedHandler(http.StatusOK, `{}`)
+			ctx.SMWithBasic.PUT(smBrokerURL+"/v2/service_instances/"+SID).WithHeader(brokerAPIVersionHeaderKey, brokerAPIVersionHeaderValue).
+				WithJSON(provisionRequestBodyMap()()).Expect().Status(http.StatusOK)
+
+			ctx.SMWithOAuth.List(web.ServiceInstancesURL).Path("$[*].id").Array().Contains(SID)
+		}, testTimeout)
+	})
+
+	Context("when provision takes more than the configured timeout", func() {
+		It("should fail with 500", func() {
+			done := make(chan struct{}, 1)
+			brokerServer.ServiceInstanceHandler = slowResponseHandler(3, done)
+			ctx.SMWithBasic.PUT(smBrokerURL+"/v2/service_instances/"+SID2).WithHeader(brokerAPIVersionHeaderKey, brokerAPIVersionHeaderValue).
+				WithJSON(provisionRequestBodyMap()()).Expect().Status(http.StatusInternalServerError)
+
+			ctx.SMWithOAuth.List(web.ServiceInstancesURL).Path("$[*].id").Array().NotContains(SID2)
+
+			verifyOperationDoesNotExist(SID2, "create")
+		}, testTimeout)
+	})
+})
