ba9beb9e38e502552f2a29a04d4aae6cd852858c
epsilontal <epsilontal@live.com>
2021-04-12 23:14:52 +0300

    Minor change for create catalog flow & reference-plan

--- storage/interceptors/broker_catalog_helper.go
+++ storage/interceptors/broker_catalog_helper.go
@@ -1,12 +1,16 @@
 package interceptors
 
-import "github.com/Peripli/service-manager/pkg/types"
+import (
+	"fmt"
+	"github.com/Peripli/service-manager/pkg/types"
+	"github.com/gofrs/uuid"
+)
 
 func GenerateReferencePlanForShareableOfferings(catalogServices []*types.ServiceOffering, catalogPlansMap map[string][]*types.ServicePlan) {
 	for _, service := range catalogServices {
-		for _, servicePlan := range service.Plans {
-			if servicePlan.IsShareablePlan() {
-				referencePlan := generateReferencePlanObject(servicePlan.ServiceOfferingID)
+		for _, plan := range service.Plans {
+			if plan.IsShareablePlan() {
+				referencePlan := generateReferencePlanObject(plan.ServiceOfferingID)
 				service.Plans = append(service.Plans, referencePlan)
 				// When not on "update catalog" flow:
 				if catalogPlansMap != nil {
@@ -16,4 +20,21 @@ func GenerateReferencePlanForShareableOfferings(catalogServices []*types.Service
 			}
 		}
 	}
+}
+
+func generateReferencePlanObject(serviceOfferingId string) *types.ServicePlan {
+	referencePlan := new(types.ServicePlan)
+	identity := "reference-plan"
+	referencePlan.CatalogName = identity
+	referencePlan.ServiceOfferingID = serviceOfferingId
+	referencePlan.Name = identity
+	referencePlan.Description = "Reference plan"
+	UUID, err := uuid.NewV4()
+	if err != nil {
+		panic(fmt.Errorf("could not generate GUID for ServicePlan: %s", err))
+	}
+	referencePlan.ID = UUID.String()
+	referencePlan.CatalogID = UUID.String()
+
+	return referencePlan
 }
--- storage/interceptors/broker_create_catalog_interceptor.go
+++ storage/interceptors/broker_create_catalog_interceptor.go
@@ -142,19 +142,3 @@ func brokerCatalogAroundTx(ctx context.Context, broker *types.ServiceBroker, fet
 
 	return nil
 }
-func generateReferencePlanObject(serviceOfferingId string) *types.ServicePlan {
-	referencePlan := new(types.ServicePlan)
-	identity := "reference-plan"
-	referencePlan.CatalogName = identity
-	referencePlan.ServiceOfferingID = serviceOfferingId
-	referencePlan.Name = identity
-	referencePlan.Description = "Reference plan"
-	UUID, err := uuid.NewV4()
-	if err != nil {
-		panic(fmt.Errorf("could not generate GUID for ServicePlan: %s", err))
-	}
-	referencePlan.ID = UUID.String()
-	referencePlan.CatalogID = UUID.String()
-
-	return referencePlan
-}
