298e8cbfaf2a3de0e486c32ea4cf136bd5b26328
Ido Bergerfroind <ido.bergerfroind@sap.com>
2021-05-11 10:48:24 +0300

    upgrade osb version to 2.14

--- test/service_instance_and_binding_test/service_binding_test/service_binding_test.go
+++ test/service_instance_and_binding_test/service_binding_test/service_binding_test.go
@@ -368,7 +368,7 @@ var _ = DescribeTestsFor(TestCase{
 						When("instance exists in a platform different from service manager", func() {
 							const (
 								brokerAPIVersionHeaderKey   = "X-Broker-API-Version"
-								brokerAPIVersionHeaderValue = "2.13"
+								brokerAPIVersionHeaderValue = "2.14"
 								SID                         = "abc1234"
 							)
 
