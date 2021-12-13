bfd62097a4a6b5175032b6419cb908545398317c
pavelmaliy <pavel.maliy@sap.com>
2021-12-13 22:03:24 +0200

    Update broker_client.go

--- pkg/client/broker_client.go
+++ pkg/client/broker_client.go
@@ -59,11 +59,8 @@ func (bc *BrokerClient) authAndTlsDecorator(requestHandler util.DoRequestWithCli
 		client := http.DefaultClient
 		ctx := req.Context()
 		logger := log.C(ctx)
-		if bc.broker.Credentials.BasicExists() {
-			logger.Infof("######@@@ setting basic auth %s:%s", bc.broker.Credentials.Basic.Username, bc.broker.Credentials.Basic.Password)
+		if bc.broker.Credentials.BasicExists() {			
 			bc.addBasicAuth(req)
-		} else {
-			logger.Infof("######@@@  not setting basic auth")
 		}
 
 		if bc.tlsConfig != nil {
