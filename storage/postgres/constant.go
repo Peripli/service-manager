0db55893830b95ef27112a8afba0f76c647f9849
i320494 <nir.ben.rei@sap.com>
2022-06-01 13:00:06 +0300

    add index for operations_resource_type_resource_id_index on operations table

--- storage/postgres/constant.go
+++ storage/postgres/constant.go
@@ -2,4 +2,4 @@ package postgres
 
 // todo: get automatically from folder
 // change this line with the latest version of the migrations:
-const latestMigrationVersion = "20211125100000"
+const latestMigrationVersion = "20220601100000"
