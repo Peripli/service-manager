184678d5c28308f395480026d4e26c4d363bfe8c
i314411 <michal.yaros@sap.com>
2022-07-11 11:50:24 +0300

    Add indexes to db migration scripts

---
+++ storage/postgres/migrations/20220711100000_service_offering_labels_service_offering_id_index.up.sql
@@ -0,0 +1,3 @@
+CREATE INDEX IF NOT EXISTS service_offering_labels_service_offering_id_index
+    on service_offering_labels (service_offering_id);
+
---
+++ storage/postgres/migrations/20220711100000_service_offerings_catalog_name_index.up.sql
@@ -0,0 +1,3 @@
+CREATE INDEX IF NOT EXISTS service_offerings_catalog_name_index
+    on service_offerings (catalog_name);
+
